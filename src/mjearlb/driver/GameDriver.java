867e09d7b04f3840cdc6553b5ce1bcb4902d7322
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-05-23 22:40:58 -0400

    Added some initialization methods for the player and NPC's

--- src/mjearlb/driver/GameDriver.java
+++ src/mjearlb/driver/GameDriver.java
@@ -17,7 +17,32 @@ public class GameDriver {
      * by the game during runtime.
      */
     public static void initObjects() {
-        Map map = new Map();
+        Map outside = new Map(5,5);
+        getPlayer();
+        getNPCs();
     } // initObjects
+
+    /**
+     * Finds and initializes the player's character object.
+     *
+     * TO DO: figure out how to store and retrieve the player's
+     * character object
+     */
+    public static void getPlayer() {
+        Character player = new Character();
+        player.setName("player");
+        player.setId(20);
+        player.setIsPlayer(true);
+    } // getPlayer
+
+    /**
+     * Creates all NPC's.
+     */
+    private static void getNPCs() {
+        Character ted = new Character();
+        ted.setName("Ted");
+        ted.setId(5);
+        ted.setIsPlayer(false);
+    } // getNPCs
 
 } // GameDriver
