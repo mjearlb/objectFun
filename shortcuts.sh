31e2c70dae72b7aa096dc9a7702b45bd8f052de9
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-05-23 22:22:24 -0400

    Added shortcuts for GameDriver and Map

---
+++ shortcuts.sh
@@ -0,0 +1,17 @@
+echo
+
+echo emacs src/mjearlb/game/Character.java
+
+echo
+
+echo emacs src/mjearlb/game/Map.java
+
+echo
+
+echo emacs src/mjearlb/driver/CharacterCreation.java
+
+echo
+
+echo emacs src/mjearlb/driver/GameDriver.java
+
+echo
