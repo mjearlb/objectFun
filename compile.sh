a34ffed338431c63a9b186f2288377da4c680fcd
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-05-23 22:22:03 -0400

    Added compilation for GameDriver

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+javac -d bin src/mjearlb/game/Character.java
+
+javac -d bin src/mjearlb/game/Map.java
+
+javac -d bin -cp bin src/mjearlb/driver/CharacterCreation.java
+
+javac -d bin -cp bin src/mjearlb/driver/GameDriver.java
