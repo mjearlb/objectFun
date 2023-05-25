7aa7039b73d4bd4f36876c4948f29dfb5422e3a8
administrator <administrator@MC-C02QT1K8G8WL.local>
2023-05-25 12:28:03 -0400

    Created TestMap class

---
+++ src/mjearlb/maps/TestMap.java
@@ -0,0 +1,13 @@
+package mjearlb.maps;
+
+import mjealrb.game.Map;
+
+public class TestMap extends Map {
+
+    // TOD): populate with NPC's, build map layout, etc
+
+    public TestMap() {
+	super(); 
+    } // TestMap
+
+} // Map
