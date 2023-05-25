ea4678838237bfab2a26954637ff81469888b6a4
administrator <administrator@MC-C02QT1K8G8WL.local>
2023-05-25 11:52:55 -0400

    Added NPC to compile.sh

--- compile.sh
+++ compile.sh
@@ -4,6 +4,8 @@ javac -d bin src/mjearlb/game/Character.java
 
 javac -d bin -cp bin src/mjearlb/game/Player.java
 
+javac -d bin -cp bin src/mjearlb/game/NonPlayableCharacter.java
+
 javac -d bin -cp bin src/mjearlb/game/Inventory.java
 
 javac -d bin src/mjearlb/game/Map.java
