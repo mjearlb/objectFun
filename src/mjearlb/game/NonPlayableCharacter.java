f28a3887fd7d91e9db6fcfad81cf804eb643beeb
administrator <administrator@MC-C02QT1K8G8WL.local>
2023-05-25 11:57:33 -0400

    Created NPC class with basic functionality

---
+++ src/mjearlb/game/NonPlayableCharacter.java
@@ -0,0 +1,12 @@
+package mjearlb.game;
+
+import mjealrb.game.Character;
+
+public class NonPlayableCharacter extends Character {
+
+    public NonPlayableCharacter() {
+	super();
+	this.isPlayer = false; 
+    } // NonPlayableCharacter
+
+} // NonPlayableCharacter
