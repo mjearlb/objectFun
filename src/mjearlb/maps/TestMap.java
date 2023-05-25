ab1db55fb22b5bdb0a2d1e4f909b1c3ea3fd7b1d
administrator <administrator@MC-C02QT1K8G8WL.local>
2023-05-25 13:01:04 -0400

    Added some functionality to TestMap

--- src/mjearlb/maps/TestMap.java
+++ src/mjearlb/maps/TestMap.java
@@ -1,13 +1,43 @@
 package mjearlb.maps;
 
 import mjearlb.game.Map;
+import mjearlb.game.NonPlayableCharacter; 
 
 public class TestMap extends Map {
 
-    // TOD): populate with NPC's, build map layout, etc
+    // TODO: populate with NPC's, build map layout, etc
+    // map NPC's to ints (with hashmap?) and then build the map
+    // where each int on map refers to the NPC. Will need to make the
+    // hashmap public access global variable. 
+
+    NonPlayableCharacter[] npcs;
 
     public TestMap() {
-	super(5,5); 
+	super(5,5); // should set it to an empty map of size (rows, cols)
+	initNPCs();
+	buildMap();
+	hashNPCs(); 
     } // TestMap
 
-} // Map
+    private void initNPCs() {
+	String[] names = {"Tim", "Nathan", "Claire", "Kevin", "Supa Mike");
+	int id = 5; 
+	npcs = new NonPlayableCharacter[5];
+	for (int i = 0; i < 5; i++) {
+	    npcs[i] = new NonPlayableCharacter();
+	    npcs[i].setName(names[i]);
+	    npcs[i].setId(id);
+	    id++; 
+	} // for
+
+    } // initNPCs
+
+    private void buildMap() {
+	this.map[0][0] = 1; 
+    } // buildMap
+
+    private void hashNPCs() {
+	System.out.println("Cannot map NPC's to int's"); 
+    } // hashNPCs
+
+} // TestMap
