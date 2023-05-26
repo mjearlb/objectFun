f6049b1e8fc983cc38b9b7f788db8850a0444bd0
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-05-25 22:45:16 -0400

    Added constructor and ID to Item class

--- src/mjearlb/game/items/Item.java
+++ src/mjearlb/game/items/Item.java
@@ -1,9 +1,46 @@
 package mjearlb.game.items;
 
+/**
+ * Is an object that can be placed in a
+ * character's inventory.
+ */
 public class Item extends Object {
 
-    public Item() {
+    private int id;
 
+    /**
+     * Creates an Item Object with no id. .
+     */
+    public Item() {
+        super();
     } // Item
+
+    /**
+     * Creates an Item Object.
+     *
+     * @param id the id of the item.
+     */
+    public Item(int id) {
+        super();
+        this.id = id;
+    } // Item
+
+    /**
+     * Allows for changing of the item id.
+     *
+     * @param id the id of the item.
+     */
+    public void setId(int id) {
+        this.id = id;
+    } // setId
+
+    /**
+     * Allows for retrieval of the item's id.
+     *
+     * @return the {@code id} of the item.
+     */
+    public int getId() {
+        return this.id;
+    } // getId
 
 } // Item
