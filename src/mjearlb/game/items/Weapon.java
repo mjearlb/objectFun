93390dab5b0f4ed975b850a65f264f193e46e59b
administrator <milobauman@gmail.com>
2023-06-01 08:02:02 -0400

    Added Weapon class

---
+++ src/mjearlb/game/items/Weapon.java
@@ -0,0 +1,13 @@
+package mjearlb.game.items;
+
+import mjearlb.game.items.Item;
+
+public class Weapon extends Item {
+
+    int damage; 
+
+    public Weapon() {
+	super(); 
+    } // Weapon
+
+} // Weapon
