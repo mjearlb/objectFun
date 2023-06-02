cb8908cbc5b3d5c85870c80fcf231cd12140320b
administrator <milobauman@gmail.com>
2023-06-02 15:08:55 -0400

    Added wizard, human, and elf options to Stats class

--- src/mjearlb/game/character/Stats.java
+++ src/mjearlb/game/character/Stats.java
@@ -9,9 +9,43 @@ public class Stats {
     int strength;
     int wisdom;
     int agility;
+    int deception;
+    int magic; 
 
-    public Stats() {
-
+    /**
+     * Creates a character's stats. 
+     *
+     * @param charClass the class of the character. 
+     */
+    public Stats(String charClass) {
+	setStats(charClass); 
     } // Stats
+
+    /**
+     * Sets a character's stats based on what class the character is. 
+     *
+     * @param charClass the class of the character. 
+     */
+    private void setStats(String charClass) {
+	if (charClass.equals("wizard")) {
+	    this.stregth = 6; 
+	    this.wisdom = 12; 
+	    this.agility = 6; 
+	    this.deception = 10; 
+	    this.magic = 16; 
+	} else if (charClass.equals("human")) {
+	    this.stregth = 8; 
+            this.wisdom	= 8; 
+            this.agility = 10; 
+            this.deception = 8; 
+            this.magic = 4; 
+	} else if (charClass.equals("elf")) {
+	    this.stregth = 6; 
+            this.wisdom	= 11; 
+            this.agility = 13; 
+            this.deception = 9; 
+	    this.magic = 14; 
+	} // if/else
+    } // setStats
 
 } // Stats
