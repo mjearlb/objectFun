4aff5a904052cc7a0e49ac3e1c5b02cd94bc3757
administrator <milobauman@gmail.com>
2023-06-01 22:19:08 -0400

    Added Chest class

---
+++ src/mjearlb/game/Chest.java
@@ -0,0 +1,12 @@
+package mjearlb.game;
+
+import java.util.LinkedList;
+import mjearlb.game.items.Item;
+
+public class Chest extends LinkedList {
+
+    public Chest() {
+
+    } // Chest
+
+} // Chest
