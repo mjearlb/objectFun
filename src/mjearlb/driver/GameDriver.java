bc4efba4f832c438547e2657e3f5204724953a30
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-06-18 08:40:16 -0400

    Fixed indentation and prints player character after choosing them

--- src/mjearlb/driver/GameDriver.java
+++ src/mjearlb/driver/GameDriver.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import mjearlb.maps.TestMap;
 import mjearlb.game.Map;
 import mjearlb.game.character.Player;
-import mjearlb.game.character.Stats; 
+import mjearlb.game.character.Stats;
 import mjearlb.game.character.NonPlayableCharacter;
 import mjearlb.writer.ReadFromFile;
 import static mjearlb.writer.ReadFromFile.readFromFile;
@@ -20,16 +20,16 @@ public class GameDriver {
     private static boolean playing;
     private static String choice;
     public static TestMap map;
-    private static Player player; 
+    private static Player player;
 
     public static void main(String[] args) {
-	playing = true;
-	getPlayer(); 
+        playing = true;
+        getPlayer();
         initObjects();
-	while (playing) {
-	    choice = keyboard.nextLine();
-	    checkChoice(); 
-	} // while
+        while (playing) {
+            choice = keyboard.nextLine();
+            checkChoice();
+        } // while
     } // main
 
     /**
@@ -41,22 +41,22 @@ public class GameDriver {
     } // initObjects
 
     /**
-     * Checks the choice that the player makes. 
+     * Checks the choice that the player makes.
      */
     private static void checkChoice() {
-	switch (choice) {
-	case "help":
-	    printHelp(); 
-	    break;
-	case "map":
-	    map.dispMap();
-	    break;
-	case "w":
-	    playerWalk(); 
-	    break; 
-	default:
-	    System.out.println("Error: not a valid option"); 
-	} // switch
+        switch (choice) {
+        case "help":
+            printHelp();
+            break;
+        case "map":
+            map.dispMap();
+            break;
+        case "w":
+            playerWalk();
+            break;
+        default:
+            System.out.println("Error: not a valid option");
+        } // switch
     } // checkChoice
 
     /**
@@ -66,66 +66,67 @@ public class GameDriver {
      * character object
      */
     private static void getPlayer() {
-	boolean choosePlayer = true;
-	String playerChoice = ""; 
-	while (choosePlayer) {
-	    System.out.println("Load character or use default?\nTo choose, type " +
-			       "\"load\" or \"default\":");
-	    playerChoice = keyboard.nextLine(); 
-	    if (playerChoice.equalsIgnoreCase("load")) {
-		boolean validName = false;
-		while (!validName) {
-		    System.out.println("What is the name of your character?");
-		    String playerName = keyboard.nextLine();
-		    try {
-			player = readFromFile(playerName, Player.class);
-			validName = true;
-			choosePlayer = false;
-			System.out.println(player); 
-		    } catch (FileNotFoundException e) {
-			System.out.println("Error: file not found"); 
-		    } // catch
-		} // while
-	    } else if (playerChoice.equalsIgnoreCase("default")) {
-		player = new Player();
-		player.setName("Dave");
-		player.stats = new Stats("human");
-		player.charClass = "human";
-	    } else {
-		System.out.println("Error: invalid choice!"); 
-	    } // if/else
-	} // while
+        boolean choosePlayer = true;
+        String playerChoice = "";
+        while (choosePlayer) {
+            System.out.println("Load character or use default?\nTo choose, type " +
+            "\"load\" or \"default\":");
+            playerChoice = keyboard.nextLine();
+            if (playerChoice.equalsIgnoreCase("load")) {
+                boolean validName = false;
+                while (!validName) {
+                    System.out.println("What is the name of your character?");
+                    String playerName = keyboard.nextLine();
+                    try {
+                        player = readFromFile(playerName, Player.class);
+                        validName = true;
+                        choosePlayer = false;
+                        System.out.println(player);
+                    } catch (FileNotFoundException e) {
+                        System.out.println("Error: file not found");
+                    } // catch
+                } // while
+            } else if (playerChoice.equalsIgnoreCase("default")) {
+                player = new Player();
+                player.setName("Dave");
+                player.stats = new Stats("human");
+                player.charClass = "human";
+            } else {
+                System.out.println("Error: invalid choice!");
+            } // if/else
+        } // while
+        System.out.println(player);
     } // getPlayer
 
     /**
-     * Prints the options available to the character. 
+     * Prints the options available to the character.
      */
     private static void printHelp() {
-	System.out.println("Help menu:\nhelp: displays valid options\n" +
-			   "w: moves up one square\n" +
-			   "s: moves down one square\n" +
-			   "a: moves left one square\n" +
-			   "d: moves right one square\n" +
-			   "map: displays the map"); 
+        System.out.println("Help menu:\nhelp: displays valid options\n" +
+            "w: moves up one square\n" +
+            "s: moves down one square\n" +
+            "a: moves left one square\n" +
+            "d: moves right one square\n" +
+            "map: displays the map");
     } // printHelp
 
     /**
      * Determines if the player can move. If so, then then runs the
-     * correct move method. 
+     * correct move method.
      */
     private static void playerWalk() {
-	switch (choice) {
-	case "w":
-	    break;
-	case "s":
-	    break;
-	case "a":
-	    break;
-	case "d":
-	    break;
-	default:
-	    System.out.println("Error: cannot walk here!"); 
-	} // switch
+        switch (choice) {
+        case "w":
+            break;
+        case "s":
+            break;
+        case "a":
+            break;
+        case "d":
+            break;
+        default:
+            System.out.println("Error: cannot walk here!");
+        } // switch
     } // playerWalk
 
 } // GameDriver
