e4a75ff4b82886902b8d2b92bce346b0182816d6
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-06-18 17:43:39 -0400

    Added functionality for player right movement

--- src/mjearlb/driver/GameDriver.java
+++ src/mjearlb/driver/GameDriver.java
@@ -148,10 +148,16 @@ public class GameDriver {
             } // try/catch
             break;
         case "d":
+            try {
+                map.walkRight(map.map[map.currCoords[0]][map.currCoords[1] + 1]);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Error: cannot walk out of bounds");
+            } // try/catch
             break;
         default:
             System.out.println("Error: cannot walk here!");
         } // switch
+        map.dispMap();
     } // playerWalk
 
 } // GameDriver
