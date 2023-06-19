fb8971152ce4399da7762684c5ff8eac58e76731
administrator <milobauman@gmail.com>
2023-06-19 17:51:04 -0400

    Added Perception skill

--- src/mjearlb/game/character/Stats.java
+++ src/mjearlb/game/character/Stats.java
@@ -10,7 +10,8 @@ public class Stats {
     int wisdom;
     int agility;
     int deception;
-    int magic; 
+    int magic;
+    int perception; 
 
     /**
      * Creates a character's stats. 
@@ -32,25 +33,29 @@ public class Stats {
 	    this.wisdom = 12; 
 	    this.agility = 6; 
 	    this.deception = 10; 
-	    this.magic = 16; 
+	    this.magic = 16;
+	    this.perception = 10; 
 	} else if (charClass.equals("human")) {
 	    this.strength = 8; 
             this.wisdom	= 8; 
             this.agility = 10; 
             this.deception = 8; 
-            this.magic = 4; 
+            this.magic = 4;
+	    this.perception = 7; 
 	} else if (charClass.equals("elf")) {
 	    this.strength = 6; 
             this.wisdom	= 11; 
             this.agility = 13; 
             this.deception = 9; 
-	    this.magic = 14; 
+	    this.magic = 14;
+	    this.perception = 10; 
 	} else if (charClass.equals("wook")) {
 	    this.strength = 12;
-	    this.wisdom = 6;
+	    this.wisdom = 4;
 	    this.agility = 12;
 	    this.deception = 10;
-	    this.magic = 2; 
+	    this.magic = 6;
+	    this.perception = 4; 
 	} // if/else
     } // setStats
 
@@ -60,7 +65,9 @@ public class Stats {
 	    "\n\tWisdom: " + this.wisdom +
 	    "\n\tAgility: " + this.agility +
 	    "\n\tDeception: " + this.deception +
-	    "\n\tMagic: " + this.magic; 
+	    "\n\tMagic: " + this.magic +
+	    "\n\tPerception: " + this.perception;
+	
     } // toString
 
 } // Stats
