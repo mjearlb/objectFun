ff83232c3ea4bcb422fd33012f3275c6137e7684
administrator <milobauman@gmail.com>
2023-06-21 15:56:59 -0400

    Added Sword.java for Sword items

---
+++ src/mjearlb/game/items/Sword.java
@@ -0,0 +1,23 @@
+package mjearlb.game.items;
+
+import mjearlb.game.items.Item; 
+import mjearlb.game.items.Weapon; 
+
+public class Sword extends Weapon {
+
+    /**
+     * Creates a {@code Sword} object.
+     * 
+     * @param name the name of the {@code Item}.
+     * @param damage the amount of damage this Sword does. 
+     */
+    public Sword(String name, int damage) {
+	super(name, damage); 
+    } // Sword
+
+    @Override
+    public String toString() {
+	return name + "\nDamage: " + damage; 
+    } // toString
+
+} // Sword
