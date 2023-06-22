c541cebb4cea1394789fce2cfe0cd5335ca77783
administrator <milobauman@gmail.com>
2023-06-21 21:48:56 -0400

    Updated Wearable to include defense stat

--- src/mjearlb/game/items/wearables/Wearable.java
+++ src/mjearlb/game/items/wearables/Wearable.java
@@ -4,13 +4,17 @@ import mjearlb.game.items.Item;
 
 public abstract class Wearable extends Item {
 
+    int defense; 
+
     /**
      * Creates a wearable item. 
      *
-     * @param name the name of the item. 
+     * @param name the name of the item.
+     * @param defense the amount of defense that the item provided. 
      */
-    public Wearable(String name) {
-	super(name); 
+    public Wearable(String name, int defense) {
+	super(name);
+	this.defense = defense; 
     } // Wearable
 
 } // Wearable
