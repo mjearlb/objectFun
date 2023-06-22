b7f21fff60915954d8a59e9578fae14cded3e9f5
administrator <milobauman@gmail.com>
2023-06-22 10:42:07 -0400

    Reordered compile.sh

--- compile.sh
+++ compile.sh
@@ -4,23 +4,23 @@ rm -rf bin/mjearlb
 
 javac -d bin src/mjearlb/game/character/Stats.java
 
+javac -d bin src/mjearlb/game/items/Item.java
+
+javac -d bin -cp bin src/mjearlb/game/Inventory.java
+
 javac -d bin -cp bin src/mjearlb/game/character/Character.java
 
 javac -d bin -cp bin src/mjearlb/game/character/Player.java
 
 javac -d bin -cp bin src/mjearlb/game/character/NonPlayableCharacter.java
 
-javac -d bin src/mjearlb/game/items/Item.java
+javac -d bin -cp bin src/mjearlb/game/Chest.java
 
 javac -d bin -cp bin src/mjearlb/game/items/Weapon.java
 
 javac -d bin -cp bin src/mjearlb/game/items/Sword.java
 
 javac -d bin -cp bin src/mjearlb/game/items/wearables/Wearable.java
-
-javac -d bin -cp bin src/mjearlb/game/Inventory.java
-
-javac -d bin -cp bin src/mjearlb/game/Chest.java
 
 javac -d bin src/mjearlb/maps/Map.java
 
