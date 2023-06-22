12d71ae1c40b0630949ebfd78fa33d2556e9934f
administrator <milobauman@gmail.com>
2023-06-22 12:26:45 -0400

    Added equipment Inventory to Player class. Added methods to equip/unequip Wearable Items

--- src/mjearlb/game/character/Player.java
+++ src/mjearlb/game/character/Player.java
@@ -7,6 +7,8 @@ import mjearlb.game.character.Character;
  */
 public class Player extends Character {
 
+    Inventory<Wearable> equipment; 
+
     /**
      * Creates an instance of the Player character.
      * Defaults to 10 inventory slots. 
@@ -14,6 +16,27 @@ public class Player extends Character {
     public Player() {
 	super(10); 
 	this.isPlayer = true;
+	equipment = new Inventory<Wearable>(4); 
     } // Player
+
+    /**
+     * Allows {@code Player} to equip a {@code Wearable} {@code Item}.
+     *
+     * @param item the {@code item} to be equipped. 
+     */
+    public void equipWearable(Wearable item) {
+	this.inventory.remove(item);
+	this.equipment.add(item); 
+    } // wearItem
+
+    /**
+     * Allows {@code Player} to unequip a {@code Wearable} {@code Item}.
+     *
+     * @param item the {@code item} to be unequipped. 
+     */
+    public void unequipWearable(Wearable item) {
+	this.equipment.remove(item);
+	this.inventory.add(item);
+    } // unwearItem
 
 } // Player
