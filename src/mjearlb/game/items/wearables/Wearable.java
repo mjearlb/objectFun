912324e2057cf87427dc583b26274a316c3c47a8
administrator <milobauman@gmail.com>
2023-06-21 21:43:21 -0400

    Created the Wearable abstract class. To be used to create wearable items (clothing, armor, etc.)

---
+++ src/mjearlb/game/items/wearables/Wearable.java
@@ -0,0 +1,16 @@
+package mjearlb.game.items.wearables;
+
+import mjearlb.game.items.Item;
+
+public abstract class Wearable extends Item {
+
+    /**
+     * Creates a wearable item. 
+     *
+     * @param name the name of the item. 
+     */
+    public Wearable(String name) {
+	super(name); 
+    } // Wearable
+
+} // Wearable
