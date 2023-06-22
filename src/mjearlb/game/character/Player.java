6d813eab359270db36ad8c690f10566f99c0d33c
administrator <milobauman@gmail.com>
2023-06-22 10:54:13 -0400

    Updated constructor to create the Inventory with 10 slots

--- src/mjearlb/game/character/Player.java
+++ src/mjearlb/game/character/Player.java
@@ -7,8 +7,12 @@ import mjearlb.game.character.Character;
  */
 public class Player extends Character {
 
+    /**
+     * Creates an instance of the Player character.
+     * Defaults to 10 inventory slots. 
+     */
     public Player() {
-	super();
+	super(10); 
 	this.isPlayer = true;
     } // Player
 
