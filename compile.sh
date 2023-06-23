ea98c445e284921e38e4bd36e681d30090ce1b87
administrator <milobauman@gmail.com>
2023-06-23 07:38:56 -0400

    Updated compile.sh

--- compile.sh
+++ compile.sh
@@ -6,21 +6,23 @@ javac -d bin src/mjearlb/game/character/Stats.java
 
 javac -d bin src/mjearlb/game/items/Item.java
 
-javac -d bin -cp bin src/mjearlb/game/Inventory.java
+javac -d bin -cp bin src/mjearlb/game/containers/Container.java
+
+javac -d bin -cp bin src/mjearlb/game/containers/Inventory.java
 
 javac -d bin -cp bin src/mjearlb/game/character/Character.java
+
+javac -d bin -cp bin src/mjearlb/game/items/wearables/Wearable.java
 
 javac -d bin -cp bin src/mjearlb/game/character/Player.java
 
 javac -d bin -cp bin src/mjearlb/game/character/NonPlayableCharacter.java
 
-javac -d bin -cp bin src/mjearlb/game/Chest.java
+javac -d bin -cp bin src/mjearlb/game/containers/Chest.java
 
 javac -d bin -cp bin src/mjearlb/game/items/Weapon.java
 
 javac -d bin -cp bin src/mjearlb/game/items/Sword.java
-
-javac -d bin -cp bin src/mjearlb/game/items/wearables/Wearable.java
 
 javac -d bin src/mjearlb/maps/Map.java
 
