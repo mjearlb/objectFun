797805d0d339f3d5980e5f6818ad890871869547
administrator <milobauman@gmail.com>
2023-06-27 11:49:16 -0400

    Added Helmet class with basic functionality

---
+++ src/mjearlb/game/items/wearables/Helmet.java
@@ -0,0 +1,9 @@
+package mjearlb.game.items.wearables;
+
+public class Helmet extends Wearable {
+
+    public Helmet(String name, int defense) {
+	super(name, defense); 
+    } // Helmet
+
+} // Helmet
