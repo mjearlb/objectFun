12ccb758627bafe81e85a1dd2ef2b83a73a2c75b
administrator <milobauman@gmail.com>
2023-06-27 11:59:32 -0400

    Altered the map slightly and added temporary way to differentiate the chests

--- src/mjearlb/maps/TestMap.java
+++ src/mjearlb/maps/TestMap.java
@@ -9,7 +9,8 @@ import mjearlb.game.items.Item;
 import mjearlb.game.containers.Chest;
 import mjearlb.game.items.Sword;
 import mjearlb.game.character.Player;
-import mjearlb.game.containers.Inventory; 
+import mjearlb.game.containers.Inventory;
+import mjearlb.game.items.wearables.Helmet; 
 
 /**
  * This is an example map implementation.
@@ -24,8 +25,9 @@ public class TestMap extends Map {
     private static Scanner keyboard = new Scanner(System.in);
     NonPlayableCharacter[] npcs;
     Chest<Item>[] chests;
-    Sword dullSword;
-
+    public Sword dullSword;
+    public Helmet rustyHelm; 
+    
     /**
      * This constructs the TestMap object and makes it.
      */
@@ -45,13 +47,10 @@ public class TestMap extends Map {
      */
     private void initNPCs() {
         String[] names = {"Tim", "Nathan", "Claire", "Kevin", "Supa Mike"};
-        int id = 5000;
         npcs = new NonPlayableCharacter[5];
         for (int i = 0; i < 5; i++) {
             npcs[i] = new NonPlayableCharacter(5);
             npcs[i].setName(names[i]);
-            npcs[i].setId(id);
-            id++;
         } // for
 
     } // initNPCs
@@ -60,8 +59,9 @@ public class TestMap extends Map {
      * This populates the map.
      */
     private void buildMap() {
-        this.map[1][1] = '1';
+        this.map[1][7] = '1';
 	this.map[3][3] = '0' - 1;
+	this.map[0][0] = 'C'; 
 
 	// walled off area
 	this.map[4][4] = 'W';
@@ -128,10 +128,20 @@ public class TestMap extends Map {
             System.out.println("You enter the door.");
             break;
         case 'C':
-            System.out.println("You open the chest.\n" + chests[0] + "\nTake an item? (y/n)");
-	    if (keyboard.nextLine().equals("y")) {
-	        takeFromChest(player, chests[0], dullSword); 
-	    } // if 
+	    /* NOTE: This is to be changed as it is a very bad way to distinguish between the chests.
+	       TODO: decide if Hashmap would work for this?
+	     */ 
+	    if (currCoords[0] == 1 && currCoords[1] == 7) {
+		System.out.println("You open the chest.\n" + chests[0] + "\nTake an item? (y/n)");
+		if (keyboard.nextLine().equals("y")) {
+		    takeFromChest(player, chests[0], dullSword); 
+		} // if
+	    } else if (currCoords[0] == 0 && currCoords[1] == 0) {
+		System.out.println("You open the chest.\n" + chests[1] + "\nTake an item? (y/n)");
+		if (keyboard.nextLine().equals("y")) {
+                    takeFromChest(player, chests[1], rustyHelm);
+                } // if
+	    } // else/if
             break;
         default:
             System.out.println("There is nothing for you to interact with.");
@@ -142,18 +152,20 @@ public class TestMap extends Map {
      * Initializes the in-game items. 
      */
     private void initItems() {
-	dullSword = new Sword("Sword of Dullness", 4); 
+	dullSword = new Sword("Sword of Dullness", 4);
+	rustyHelm = new Helmet("Helmet of Rustiness", 2); 
     } // initItems
 
     /**
      * Initializes the chests used in the game. 
      */
     private void initChests() {
-	chests = new Chest[1];
+	chests = new Chest[2];
 	for (int i = 0; i < chests.length; i++) {
 	    chests[i] = new Chest<Item>(10);
-	    chests[i].add(dullSword); 
-	} // for	
+	} // for
+	chests[0].add(dullSword);
+	chests[1].add(rustyHelm); 
     } // initChests
 
     /**
@@ -167,6 +179,6 @@ public class TestMap extends Map {
     public void takeFromChest(Player player, Chest chest, Item item) {
         chest.remove(item);
 	player.inventory.add(item); 
-    } // takeFromChest  
+    } // takeFromChest
 
 } // TestMap
