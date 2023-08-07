701f5ed834031cf5d950397dd5556bb956f0c62c
administrator <milobauman@gmail.com>
2023-08-07 09:05:28 -0400

    Added javadocs, changeCoords method, and made row and col public variables

--- src/mjearlb/maps/Coordinates.java
+++ src/mjearlb/maps/Coordinates.java
@@ -1,13 +1,33 @@
 package mjearlb.maps;
 
+/**
+ * Keeps track of coordinates for the Map class. 
+ */
 public class Coordinates {
 
-    int row;
-    int col; 
+    public int row;
+    public int col; 
 
+    /**
+     * Creates a Coordinates object.
+     *
+     * @param row the row.
+     * @param col the column. 
+     */
     public Coordinates(int row, int col) {
 	this.row = row;
 	this.col = col; 
     } // Coordinates
+
+    /**
+     * Changes both coordinates at once.
+     *
+     * @param row the new row value.
+     * @param col the new column value. 
+     */
+    public void changeCoords(int row, int col) {
+	this.row = row;
+        this.col = col;
+    } // changeCoords
 
 } // Coordinates
