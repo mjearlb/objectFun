09163af2f0ab4be83475b80312dd8e491726466a
administrator <milobauman@gmail.com>
2023-08-07 08:38:56 -0400

    Added Coordinates class

---
+++ src/mjearlb/maps/Coordinates.java
@@ -0,0 +1,13 @@
+package mjearlb.maps;
+
+public class Coordinates {
+
+    int row;
+    int col; 
+
+    public Coordinates(int row, int col) {
+	this.row = row;
+	this.col = col; 
+    } // Coordinates
+
+} // Coordinates
